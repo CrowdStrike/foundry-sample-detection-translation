a86a67c3224d86980f0c795405db6fccd5f87653
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 14:12:41 -0600

    Trim unused fixtures from e2e/src/fixtures.ts

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,39 +1,19 @@
 import { test as baseTest } from '@playwright/test';
-import {
-  FoundryHomePage,
-  AppCatalogPage,
-  SocketNavigationPage,
-  config,
-} from '@crowdstrike/foundry-playwright';
+import { SocketNavigationPage } from '@crowdstrike/foundry-playwright';
 import { DetectionContextExplorerPage } from './pages/DetectionContextExplorerPage';
 
 type FoundryFixtures = {
-  foundryHomePage: FoundryHomePage;
-  appCatalogPage: AppCatalogPage;
   detectionContextExplorerPage: DetectionContextExplorerPage;
   socketNavigationPage: SocketNavigationPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  foundryHomePage: async ({ page }, use) => {
-    await use(new FoundryHomePage(page));
-  },
-
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
   detectionContextExplorerPage: async ({ page }, use) => {
     await use(new DetectionContextExplorerPage(page));
   },
 
   socketNavigationPage: async ({ page }, use) => {
     await use(new SocketNavigationPage(page));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
   },
 });
 
